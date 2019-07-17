eb44b4fd640d0f77dfc19c3cefe6c9353aeaacee
coco <zoross_@naver.com>
2019-07-17 19:51:40 +0900

    MeditSmile2D - DataContext Binding (in Code behind) 구현 - ToothTemplate(UserControl type) 추가

---
+++ MeditSmile2D/MeditSmile2D/View/ToothTemplate.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MeditSmile2D.View
+{
+    /// <summary>
+    /// ToothTemplate.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ToothTemplate : UserControl
+    {
+        public ToothTemplate()
+        {
+            InitializeComponent();
+
+            Binding binding = new Binding("Main");
+            binding.Source = FindResource("Locator");
+            SetBinding(DataContextProperty, binding);
+        }
+    }
+}
---
+++ MeditSmile2D/MeditSmile2D/View/Utils/ViewUtils.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace MeditSmile2D.View.Utils
+{
+    public static class ViewUtils
+    {
+        // do you have any parent?
+        public static bool AnyParent(DependencyObject item, Func<DependencyObject, bool> condition)
+        {
+            if (item == null)
+                return false;
+
+            var logicalParent = LogicalTreeHelper.GetParent(item);
+            var visualParent = VisualTreeHelper.GetParent(item);
+
+            return condition(item) || AnyParent(visualParent, condition);
+        }
+
+        // Get Above Parent
+        public static DependencyObject GetParent(DependencyObject item, Func<DependencyObject, bool> condition)
+        {
+            if (item == null)
+                return null;
+
+            var logicalParent = LogicalTreeHelper.GetParent(item);
+            var visualParent = VisualTreeHelper.GetParent(item);
+
+            return condition(item) ? item : GetParent(visualParent, condition);
+        }
+
+        public static DependencyObject GetVisualChild(DependencyObject item, Func<DependencyObject, bool> condition)
+        {
+            if (item == null)
+                return null;
+
+            var q = new Queue<DependencyObject>();
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(item); i++)
+            {
+                var child = VisualTreeHelper.GetChild(item, i);
+                if (condition(child))
+                    return child;
+                q.Enqueue(child);       // condition(child) == false인 Child들을 모음
+            }
+
+            // condition(t) == false였던 Child들을 다시 각자 파고들어서 다시 검색
+            while (q.Count > 0)
+            {
+                var subChild = q.Dequeue();
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(subChild); i++)
+                {
+                    var childofchild = VisualTreeHelper.GetChild(subChild, i);
+                    if (condition(childofchild))
+                        return childofchild;
+                    q.Enqueue(childofchild);
+                }
+            }
+
+            // 두단계나 검색했는데 없으면 Child가 없는 것으로 리턴
+            return null;
+        }
+
+        public static DependencyObject GetLogicalChild(DependencyObject item, Func<DependencyObject, bool> condition)
+        {
+            if (item == null)
+                return null;
+
+            var q = new Queue<DependencyObject>();
+            foreach (var child in LogicalTreeHelper.GetChildren(item))
+            {
+                var dependencyChild = child as DependencyObject;
+                if (condition(dependencyChild))
+                    return dependencyChild;
+                q.Enqueue(dependencyChild);
+            }
+
+            while (q.Count > 0)
+            {
+                var subChild = q.Dequeue();
+                foreach (var childofchild in LogicalTreeHelper.GetChildren(subChild))
+                {
+                    var dependencyChildofChild = childofchild as DependencyObject;
+                    if (condition(dependencyChildofChild))
+                        return dependencyChildofChild;
+                    q.Enqueue(dependencyChildofChild);
+                }
+            }
+
+            return null;
+        }
+    }
+}
