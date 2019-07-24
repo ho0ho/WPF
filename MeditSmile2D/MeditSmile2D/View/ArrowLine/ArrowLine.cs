8c4ad3027667eb3c7a81ffa0e3ff6c8c9907882f
coco <zoross_@naver.com>
2019-07-24 21:27:57 +0900

    Medit Smile 2D - ArrowLine 기능 추가중

---
+++ MeditSmile2D/MeditSmile2D/View/ArrowLine/ArrowLine.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace MeditSmile2D.View.ArrowLine
+{
+    class ArrowLine : ArrowLineBase
+    {
+        public static readonly DependencyProperty X1Property =
+            DependencyProperty.Register("X1",
+                typeof(double), typeof(ArrowLine),
+                new FrameworkPropertyMetadata(0.0,
+                        FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        public double X1
+        {
+            set { SetValue(X1Property, value); }
+            get { return (double)GetValue(X1Property); }
+        }
+
+        public static readonly DependencyProperty Y1Property =
+            DependencyProperty.Register("Y1",
+                typeof(double), typeof(ArrowLine),
+                new FrameworkPropertyMetadata(0.0,
+                        FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        public double Y1
+        {
+            set { SetValue(Y1Property, value); }
+            get { return (double)GetValue(Y1Property); }
+        }
+
+        public static readonly DependencyProperty X2Property =
+            DependencyProperty.Register("X2",
+                typeof(double), typeof(ArrowLine),
+                new FrameworkPropertyMetadata(0.0,
+                        FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        public double X2
+        {
+            set { SetValue(X2Property, value); }
+            get { return (double)GetValue(X2Property); }
+        }
+
+        public static readonly DependencyProperty Y2Property =
+            DependencyProperty.Register("Y2",
+                typeof(double), typeof(ArrowLine),
+                new FrameworkPropertyMetadata(0.0,
+                        FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        public double Y2
+        {
+            set { SetValue(Y2Property, value); }
+            get { return (double)GetValue(Y2Property); }
+        }
+
+        protected override Geometry DefiningGeometry
+        {
+            get
+            {
+                // Clear out the PathGeometry.
+                pathgeo.Figures.Clear();
+
+                // Define a single PathFigure with the points.
+                pathfigLine.StartPoint = new Point(X1, Y1);
+                polysegLine.Points.Clear();
+                polysegLine.Points.Add(new Point(X2, Y2));
+                pathgeo.Figures.Add(pathfigLine);
+
+                // Call the base property to add arrows on the ends.
+                return base.DefiningGeometry;
+            }
+        }
+    }
+}
